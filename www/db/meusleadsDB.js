ad007d3ec98910da5b3306665cd6fa9788b5d0a3
Vinicius <Viniciusblink@hotmail.com>
2019-02-13 13:59:45 -0200

    cmmt3

--- www/db/meusleadsDB.js
+++ www/db/meusleadsDB.js
@@ -29,7 +29,9 @@ var MEUSLEADS_DB = {
             type: 'POST',
             dataType: 'json',
             data:{ 'idUsuario': idUsuario, 'email': email, 'registroDeDispositivo': serial },
-            beforeSend: function(){ },
+            beforeSend: function(){
+                $("#deviceready").append('<img class="loader" src="./img/loader.gif">');
+            },
             complete: function(){ },
             success: function(resp){
                 var table="";
@@ -39,16 +41,19 @@ var MEUSLEADS_DB = {
                             table +='<div class="row card-header">';
                                 table +='<div class="col-6 col-md-6 p-0 "><h5 class="title ellipsis" ><i class="far fa-envelope icon-lead"></i>'+ lead.nome +'</h5></div>';
                                 table +='<div class="col-3 col-md-3 p-0 border-left border-right "><small class="small bold">DDD: '+ lead.dddTel +' </small></div>';
-                                table +='<div class="col-2 col-md-2 p-0 "><i class="fas '+((lead.idTipo==3) ? 'fa-user-tie empresarial' : ((lead.idTipo==2 ) ? 'fa-users familiar' : 'fa-user individual') )+'  "></i></div>';
+                                var tipo = ((lead.idTipo==3) ? 'EMP' : ((lead.idTipo==2 ) ? 'FAM' : 'IND') );
+
+                                table +='<div class="col-2 col-md-2 p-0 "><i class="fas '+((lead.idTipo==3) ? 'fa-user-tie empresarial' : ((lead.idTipo==2 ) ? 'fa-users familiar' : 'fa-user individual') )+'  "></i> <small class="tipoitemsmall '+tipo+'" >'+tipo+'</small></div>';
                                 table +='<div class="col-1 col-md-1 p-0 border-left actionOpenLeadBox "><i class="fas fa-angle-double-down"></i></div>';
                             table +='</div>';
-                            table +='<div class="row card-body" ></div>';
+                            table +='<div class="row card-body" style="overflow-y: auto"></div>';
                         table +='</div>';
                     });
                 }else{
                     table="<h4>Nenhum lead encontrado</h4>";
                 }
 
+                $('.loader').fadeOut().remove();
                 $('#litagemDeLeads').append(table);
 
             }
@@ -66,22 +71,63 @@ var MEUSLEADS_DB = {
             type: 'POST',
             dataType: 'json',
             data:{ 'idUsuario': usuarioL.idUsuario, 'idLead': idLead, 'registroDeDispositivo': serial },
-            beforeSend: function(){ },
+            beforeSend: function(){
+                $("#deviceready").append('<img class="loader" src="./img/loader.gif">');
+            },
             complete: function(){ },
             success: function(resp){
-                var body="";
+                var body='';
                 if(resp.type=='success'){
+                    var body='<div class="col-12 p-0" style="overflow-y: auto"><ul class="list-group">';
                     lead=resp.data;
 
-                    body +='<h5><i class="fas '+((lead.idTipo==3) ? 'fa-user-tie empresarial' : ((lead.idTipo==2 ) ? 'fa-users familiar' : 'fa-user individual') )+'  "></i>'+ lead.nome +'</h5>';
-                    body +='<p ><i class="far fa-envelope icon-lead"></i>'+ lead.email +'</p>';
-                    body +='<p ><i class="far fa-envelope icon-lead"></i>('+lead.dddTel +') - '+lead.tel+'</p>';
+                    var corTipo = (lead.idTipo == 3 ? "#d9534f" : (lead.idTipo == 2 ? "#f0ad4e" : "#5cb85c"));
+                    body += '<li class="list-group-item d-flex linha-info-lead "> <i class="fas '+((lead.idTipo==3) ? 'fa-user-tie empresarial' : ((lead.idTipo==2 ) ? 'fa-users familiar' : 'fa-user individual') )+'  icon-lead "></i> <span style="color: ' + corTipo + '">' + lead.tipo + "<span></li>";
+
+                    if(lead.operadora)
+                        body += '<li class="list-group-item d-flex linha-info-lead ">Operadora: ' +lead.operadora + '</li>';
+
+                    body +='<li class="list-group-item d-flex linha-info-lead "><i class="fas fa-address-card icon-lead"></i> '+ lead.nome +'</li>';
+
+                    if (lead.email)
+                        body += '<li class="list-group-item d-flex linha-info-lead "><i class="far fa-envelope icon-lead"></i> ' + lead.email + '</li>';
+
+                    if (lead.dddTel && lead.tel)
+                        body += '<li class="list-group-item d-flex linha-info-lead "><i class="fas fa-phone icon-lead"></i>(' + lead.dddTel + ') - ' + (lead.tel ? lead.tel : '') + '</li>';
+
+                    if (lead.dddCel && lead.cel)
+                        body += '<li class="list-group-item d-flex linha-info-lead "><i class="fas fa-mobile-alt icon-lead"></i>(' + lead.dddCel + ') - ' + (lead.cel ? lead.cel : '') + '</li>';
+
+
+                    if (lead.empresa)
+                        body += '<li class="list-group-item d-flex linha-info-lead "><i class="fas fa-building icon-lead"></i> ' + lead.empresa + '</li>';
+
+                    if (lead.cidade)
+                        body += '<li class="list-group-item d-flex linha-info-lead "><i class="fas fa-globe-americas icon-lead"></i> ' + lead.cidade + '</li>';
+
+                    if (lead.qtdVidas)
+                        body += '<li class="list-group-item d-flex linha-info-lead ">Quantidade de vidas: '+ lead.qtdVidas + '</li>';
+
+
+                    body += '<li class="list-group-item d-flex linha-info-lead "> <i class="fas fa-comments icon-lead"></i>'+ (lead.detalhes ? lead.detalhes.replace(/\n/g, "<br>") : "") + '</li>';
+
+
+                    body+='</ul></div>';
 
                 }else{
                     body="<h4>OPS!</h4>";
                 }
                 $('[data-idLead="'+lead.idLead+'"]').find('.card-body').html(body);
+                $('.loader').fadeOut().remove();
+
+                $('[data-idLead="'+lead.idLead+'"]').find('.card-header .actionOpenLeadBox .fas').removeClass('fa-angle-double-down').addClass('fa-times fecharLead');
                 $('[data-idLead="'+lead.idLead+'"]').addClass('flayCard').removeClass('cardClose');
+
+                //scroll para evitar recarregar a pagina com scrollto js
+                var top = $("#deviceready").scrollTop();
+                if(top==0){ $("#deviceready").animate({scrollTop:2}, '500'); }
+
+
 
             }
         });
